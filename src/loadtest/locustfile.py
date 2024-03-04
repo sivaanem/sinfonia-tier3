b88f4c2620e397b5f5bc8b60cb6b63c820ef5dbe
Siva Sai Anem <sanem@umass.edu>
2024-03-04 03:48:03 +0000

    [fix] merge conflict

--- src/loadtest/locustfile.py
+++ src/loadtest/locustfile.py
@@ -10,17 +10,10 @@ class MatMulUser(HttpUser):
     @task
     def matmul(self):
         self.client.post(
-<<<<<<< HEAD
             'http://obelix30.cs.umass.edu:30080/api/v1/matmul',
             json={
                 'matrix1': fake.bigmath.square_matrix(n=50),
                 'matrix2': fake.bigmath.square_matrix(n=50),
-=======
-            'http://localhost/api/v1/matmul',
-            json={
-                'matrix1': fake.bigmath.square_matrix(n=100),
-                'matrix2': fake.bigmath.square_matrix(n=100),
->>>>>>> b02f7d67c517d01d3cfdc769030beb99507954dc
                 }
             )
         
